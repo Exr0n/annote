9e6ef7a4f450858083342d1d250023e649d02146
Exr0n <spotyie@gmail.com>
2020-03-20 13:42:09 -0700

    chore: clean up code

--- main.js
+++ main.js
@@ -10,43 +10,24 @@ const edit = () => {
         markdown = markdown.replace('\n\n\n', '\n\n'); // remove duplicated newlines
         return markdown;
     }
-    // const stretchFit = (textarea) => {
-    //     var val = textarea.value;
-    //     var cols = textarea.cols;
-    //     var linecount = 0;
-    //     val.split('\n').forEach((l) => {
-    //         linecount += Math.ceil(l.length / cols);
-    //     });
-    //     textarea.rows = 2*linecount;
-    //     // textarea.display.height = '' + linecount + 'em';
-    // }
-
-    // console.log(rendered.clientHeight);
     editor.clientWidth = rendered.clientWidth;
     editor.style.height = '' + rendered.clientHeight + 'px';
     rendered.style.display = "none";
     editor.style.display = "inherit";
 
     editor.value = sanitize(rendered.innerHTML);
-    // stretchFit(editor);
 }
 const render = () => {
-    // console.log("converting MD > HTML")
-    // console.log(rendered.innerHTML)
-
     editor.style.display = "none";
     rendered.style.display = "inherit";
     rendered.innerHTML = converter.makeHtml(editor.value);
-    // content.innerHTML = marked(content.innerHTML);
 }
 
 window.onload = () => {
     rendered = document.getElementById('rendered');
     editor = document.getElementById('editor');
 
-    // rendered.addEventListener('focus', edit);
     rendered.addEventListener('click', edit);
-    // rendered.addEventListener('mouseenter', edit);
 
     editor.addEventListener('blur', render);
     editor.addEventListener('mouseleave', render);
